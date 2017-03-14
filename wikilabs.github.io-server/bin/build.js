129c0089b6cbb614e6a8775e9d5101f4c94bcd32
Mario Pietsch <pmariojo@gmail.com>
2017-03-14 16:13:28 +0100

    prepare for intro

---
+++ wikilabs.github.io-server/bin/build.js
@@ -0,0 +1,30 @@
+var fs = require('fs')
+var resolve = require('path').resolve
+var join = require('path').join
+var cp = require('child_process')
+
+// get library path
+var lib = resolve(__dirname, '../../')
+
+fs.readdirSync(lib)
+  .forEach(function (mod) {
+    var modPath = join(lib, mod)
+    // ensure path has package.json
+    if (!fs.existsSync(join(modPath, 'package.json'))) return
+
+    // check if batch processing should be skipped
+    if (fs.existsSync(join(modPath, 'skip-build'))) return
+
+    // install folder
+    console.log("\nenter directory:\n--> " + modPath + "\n--> build started\n");
+
+    cp.exec('npm run build', { env: process.env, cwd: modPath}, (e, stdout, stderr)=> {
+        if (e instanceof Error) {
+            console.error(e);
+            throw e;
+        }
+
+        if (stdout) console.log('finished: ', stdout);
+        if (stderr) console.log('error ', stderr);
+    })
+  })
